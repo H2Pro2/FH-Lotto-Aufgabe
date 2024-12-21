c6df6ca60e287e98365eadb5e4bf9de58dafb061
Michael Koncsik <michael.gothic@hotmail.com>
2024-12-21 12:24:46 +0100

    Added generate_lotto_numbers function.

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-# Michael Koncsik
+# Koncsik Michael
 
 def main():
     name = "Koncsik Michael"
     print(name)
 
+def generate_lotto_numbers(count_of_numbers, max_numbers):
+    list_of_numbers = []
+    return list_of_numbers
+
 main()
+generate_lotto_numbers(1, 2)
