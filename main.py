69e2db4088bb957072c2f2d5eb59a028b4d5d245
Michael Koncsik <michael.gothic@hotmail.com>
2024-12-21 12:57:47 +0100

    Added basic lotto logic for generate_lotto_numbers function.

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 # Koncsik Michael
 
+import random
+import sys
+
 def main():
     name = "Koncsik Michael"
     print(name)
+    print(generate_lotto_numbers(6, 45))
 
 def generate_lotto_numbers(count_of_numbers, max_numbers):
+    x = 0
     list_of_numbers = []
-    return list_of_numbers
+    while x < count_of_numbers:
+        x += 1
+        list_of_numbers.append(random.randint(1, max_numbers))
+    return list_of_numbers 
 
 main()
-generate_lotto_numbers(1, 2)
