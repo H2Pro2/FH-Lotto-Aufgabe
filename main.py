a6a7b971c224025bcaa0af90485a3d2388ac81e1
Michael Koncsik <michael.gothic@hotmail.com>
2024-12-21 13:16:09 +0100

    v2 No repeating numbers and sorted list of numbers.

--- main.py
+++ main.py
@@ -4,16 +4,20 @@ import random
 import sys
 
 def main():
-    name = "Koncsik Michael"
-    print(name)
+#    name = "Koncsik Michael"
+#    print(name)
     print(generate_lotto_numbers(6, 45))
 
 def generate_lotto_numbers(count_of_numbers, max_numbers):
     x = 0
     list_of_numbers = []
     while x < count_of_numbers:
-        x += 1
-        list_of_numbers.append(random.randint(1, max_numbers))
-    return list_of_numbers 
+        pull = random.randint(1, max_numbers)
+        if pull in list_of_numbers:
+            continue
+        else:
+            x += 1
+            list_of_numbers.append(pull)
+    return sorted(list_of_numbers)
 
 main()
